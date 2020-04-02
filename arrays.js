cd4cbe4d510dc19e54ccb6d2519ecb1dfad2308c
Jake Gonchar <jake098877@gmail.com>
2020-04-02 23:53:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   var beg = array
+  beg = [element...beg]
+  return beg;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var beg = array
   beg = beg.unshift(element)
   return beg;
 }
