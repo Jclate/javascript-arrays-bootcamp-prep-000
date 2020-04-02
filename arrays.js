b7e5bf04210a8466b3e79a8671bc1ae7060eda16
Jake Gonchar <jake098877@gmail.com>
2020-04-02 23:43:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat, skittles];
-function addElementToBeginningOfArray(number, name) {
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, name) {
   var array = [number];
   array.push(name);
 }
