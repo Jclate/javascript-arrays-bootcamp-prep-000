8c1678450ca571d39dee20c2eb15ca533610140c
Jake Gonchar <jake098877@gmail.com>
2020-04-02 23:29:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(number, name) {
-  var array = [number];
-  array.push(name);
+function addElementToBeginningOfArray(array, element) {
+  var beginning = [array];
+  beginning.push(element);
 }
