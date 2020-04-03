7653f0c3f8be4dea885ddcf4db504b8e61c405ca
Jake Gonchar <jake098877@gmail.com>
2020-04-03 00:57:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.slice(0, cats.length - 1)
+  array = array.slice(0, array.length - 1)
 }
