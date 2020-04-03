d3c6e1af22632390d86b2b3a3cbedc25dfee59ea
Jake Gonchar <jake098877@gmail.com>
2020-04-03 00:45:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var cat = element
   cat = cat.unshift(array);
-  return array;
+  return cat;
 }
 
 function addElementToEndOfArray(array, element) {
